4553320eddbcf977cd76d6501dd0c84085cfb923
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-04-14 23:57:26 +0200

    Integrate RabbitMQ to receive messages

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("test")
+import * as dotenv from 'dotenv'
+import * as amqp from 'amqplib'
+
+dotenv.config()
+
+async function main() {
+    const connection = await amqp.connect(process.env.AMQP_URL || '')
+    const channel = await connection.createChannel()
+
+    const queue = 'my_queue'
+
+    await channel.assertQueue(queue, { durable: false })
+
+    channel.consume(queue, (msg) => {
+        console.log(`Received msg: ${msg?.content.toString()}`)
+
+        if (msg) {
+            channel.ack(msg) // acknowledge msg
+        }
+    })
+}
+
+main()
